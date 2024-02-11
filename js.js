45a8ce848596785332f63a65c09e5f75c5af78e4
Teymur1234 <154793075+Teymur1234@users.noreply.github.com>
2024-02-11 12:42:23 +0400

    homework

---
+++ js.js
@@ -0,0 +1,68 @@
+//1ci sual
+// class product{
+//     constructor(count, ad, qiymet){
+//          this.say=count
+//          this.name=ad
+//          this.price=qiymet
+//     }
+// }
+// function hesabla(...products){
+//     let totalprice=0
+//     for (let i = 0; i < products.length; i++) {
+//         const product=products[i]
+//         totalprice+=product.say*product.price    
+//     }
+//     console.log("Umumi qiymet",totalprice,"AZN");
+// }
+// const cofee=new product(3,"cofee",2)
+// const tea=new product(1,"tea",1)
+// const fanta=new product(2,"fanta",2)
+// hesabla(cofee,tea,fanta)
+
+
+//2ci sual
+// function enkicik(arr) {
+//     if (!Array.isArray(arr)) {
+//         console.log("it is not array");
+//     }
+//     else {
+//         for (var i = 0; i < arr.length; i++) {
+//             for (var j = 0; j < (arr.length - i - 1); j++) {
+//                 if (arr[j] > arr[j + 1]) {
+//                     var temp = arr[j]
+//                     arr[j] = arr[j + 1]
+//                     arr[j + 1] = temp
+//                 }
+//             }
+//         }
+//         console.log(arr[0]);
+//     }
+// }
+// // const array="dksdnajsnd"
+// const array=[4,323,23,46,7,2,56,4,1]
+// enkicik(array)
+
+
+// 3cu sual
+// class animal{
+
+//     constructor(name){
+//         this.animalname=name
+//     }
+//     eat(){
+    
+//     }
+//     walk(){
+
+//     }
+//     getInfo(){
+
+//     }
+// }
+// class bird extends animal{
+          
+// }
+
+
+
+
